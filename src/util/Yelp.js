7e4ad081f6beacb0a497bf923e69cb46f6f02142
cat <marvinarivera@gmail.com>
2021-11-29 00:08:40 -0500

    completed product

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+
+const apiKey = 'CRTs3aWIqvlwjmgzSMJIqmAiWiVA2Uf0OCdhgjAIhUiK-G03N8u3Q1LOLlo9GJ67HRNSAm7uAoNfjgiThLzkO-LG1LdduyHhz51hYSmgO7aVt4B70lKsNIXw0b-fYXYx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => ({
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                }));
+            }
+        });
+    }
+};
+
+export default Yelp;
